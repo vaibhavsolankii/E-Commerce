ec85a9659b21a902b184d86087b7c7fd8edb972c
Ankita Tiwari <ankitatiwari12001@gmail.com>
2024-05-02 18:40:10 +0530

    Add carousel feature to homepage

---
+++ my-react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner from './components/Banner.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+  return (
+    <>
+      <Banner />
+    </>
+
+  );
+}
+
+export default App;
