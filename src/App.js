18f3eac9cd9ebd0da4edbb43114f39b824f8982d
Mounika Sanikireddy <r170966@rguktrkv.ac.in>
2024-05-04 17:05:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Men from "../src/components/Men";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Men />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from "./footer.module.css";
+import image1 from "../images/quality_1212289.png"
+import image2 from "../images/images.png"
 
 function Footer()
 {
@@ -13,7 +15,7 @@ function Footer()
                     <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" viewBox="0 0 576 512">
                         <path d="M559.7 392.2c17.8-13.1 21.6-38.1 8.5-55.9s-38.1-21.6-55.9-8.5L392.6 416H272c-8.8 0-16-7.2-16-16s7.2-16 16-16h16 64c17.7 0 32-14.3 32-32s-14.3-32-32-32H288 272 193.7c-29.1 0-57.3 9.9-80 28L68.8 384H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H192 352.5c29 0 57.3-9.3 80.7-26.5l126.6-93.3zm-366.1-8.3a.5 .5 0 1 1 -.9 .2 .5 .5 0 1 1 .9-.2z" />
                     </svg>
-                    <img src="images\MasterCard_Logo.svg.png" height="50px" width="30px" alt="Quality Logo" />
+                    <img src={image1} height="50px" width="30px" alt="Quality Logo" />
                 </div>
                 <div className={styles.footerSection2}>
                     <h5>EASY EXCHANGE</h5>
@@ -70,7 +72,6 @@ function Footer()
                     </div>
                 </div>
             </div>
-            <hr />
             <div className={styles.payment}>
                 <div className={styles.paymentcontainer}>
                     <div>
@@ -78,7 +79,7 @@ function Footer()
                         <div className={styles.container}>
                             <p>Net <mark>Banking</mark></p>
                             <p><i>Verified by VISA</i></p>
-                            <p><img src="images\images.png" height="40px" width="55px" alt="Payment Methods" /></p>
+                            <p><img src={image2} height="40px" width="55px" alt="Payment Methods" /></p>
                             <p>Cash on Delivery</p>
                             <p>Jio Money</p>
                         </div>
